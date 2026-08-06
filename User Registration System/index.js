54ea2113cb4a244ef365da83bdfe8f2f4d641c63
Priyank <Priyankk2405@gmail.com>
2026-08-06 08:35:56 +0530

    Update edit button

--- User Registration System/index.js
+++ User Registration System/index.js
@@ -1,35 +1,45 @@
 const express = require("express");
 const app = express();
 
-const users = []
+const users = [];
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   let usersCount = users.length;
-  res.render("index", {users, usersCount});
+  res.render("index", { users, usersCount });
 });
 
-app.get('/add', (req,res) =>{
-    res.render('addUser')
-})
+app.get("/add", (req, res) => {
+  res.render("addUser");
+});
 
-app.post('/save-user', (req,res) =>{
-     let {name, email, img} = req.body;
-     let user = {
-        name: name,
-        email: email,
-        img: img,
-     }
-     users.push(user);
-     res.redirect('/')
-})
+app.post("/save-user", (req, res) => {
+  let { name, email, img } = req.body;
+  let user = {
+    name: name,
+    email: email,
+    img: img,
+  };
+  users.push(user);
+  res.redirect("/");
+});
 
-app.post('/delete', (req, res) =>{
-     let {userId} = req.body;
-     users.splice(userId, 1);
-     res.redirect('/')
-})
+app.post("/delete", (req, res) => {
+  let { userId } = req.body;
+  users.splice(userId, 1);
+  res.redirect("/");
+});
+app.post("/edit", (req, res) => {
+  let { userId } = req.body;
+  let user = users[userId];
+  res.render("edit", { user, userId });
+});
+app.post("/update-user", (req, res) => {
+  let { userId, name, email, img } = req.body;
+  users.splice(userId, 1, { name: name, email: email, img: img });
+  res.redirect("/");
+});
 
 app.use((req, res) => {
   res.status(404).render("error");
